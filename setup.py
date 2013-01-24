26607293840ef47b0f2865b02941cd31bc66e976
Luca Bellenghi <lucabel@gmail.com>
2013-01-24 15:42:21 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.0b'
+
+tests_require = ['plone.app.testing', ]
 
 setup(name='collective.signupsheet',
       version=version,
-      description="Sign up module based on PFG and uwosh.pfg.d2c",
+      description="Signup module based on PFG and uwosh.pfg.d2c",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 4 - Beta"
         "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='signupsheet ploneformgen pfg',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://plone.org/products/collective.signupsheet',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
+      tests_require=tests_require,
+      extras_require=dict(test=tests_require),
       install_requires=[
           'setuptools',
-          'Products.PloneFormGen==1.7.6',
+          'Products.PloneFormGen',
           'uwosh.pfg.d2c'
       ],
       entry_points="""
